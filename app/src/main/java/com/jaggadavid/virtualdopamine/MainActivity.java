7bf5a00120b432cb9be8779b41ea3eb74af61291
davidJagga <djjagga@gmail.com>
2021-09-30 13:43:44 -0400

    Add remove functionality

--- app/src/main/java/com/jaggadavid/virtualdopamine/MainActivity.java
+++ app/src/main/java/com/jaggadavid/virtualdopamine/MainActivity.java
@@ -11,8 +11,10 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity{
@@ -21,7 +23,7 @@ public class MainActivity extends AppCompatActivity{
     LinearLayout list;
     ImageButton addTask;
     SharedPreferences sh;
-
+    Set<Integer> setOfIDs;
     String TAG = "com.jaggadavid.lab03.shared";
     SharedPreferences.Editor edit;
     Set<String> set= new HashSet<String>();
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity{
         sh = getSharedPreferences(TAG, MODE_PRIVATE);
         edit = sh.edit();
         set= new HashSet<String>();
-
+        setOfIDs = new HashSet<>();
         sh.getStringSet("list", set);
         setContentView(R.layout.activity_main);
         startview = findViewById(R.id.startPage);
@@ -69,6 +71,14 @@ public class MainActivity extends AppCompatActivity{
             CheckBox newCheckbox = new CheckBox(this);
             //newCheckbox.setId(R.id.basicCheckbox);
             newCheckbox.setText(s);
+            newCheckbox.setOnClickListener(this::onCheckboxClicked);
+            Random rand = new Random();
+            int x  = rand.nextInt((1000 - 100) + 1) + 100;
+            while (setOfIDs.contains(x)){
+                x  = rand.nextInt((1000 - 100) + 1) + 100;
+            }
+            newCheckbox.setId(x);
+            setOfIDs.add(x);
             newCheckbox.setMaxWidth(10000);
             list.addView(newCheckbox);
         }
@@ -79,7 +89,12 @@ public class MainActivity extends AppCompatActivity{
     public void goHome(View view){
         setContentView(R.layout.tasskdisplay);
     }
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
 
+        list = findViewById(R.id.todolist);
+        list.removeView(findViewById(view.getId()));
+    }
 
     @Override
     protected void onResume() {
